3cad37c441f2e760ada2a3bd8103f03e4b0623cc
Keshav Priyam <keshavpriyam@gmail.com>
2024-09-24 01:01:39 +0530

    pending: fix links - add subjects - add papers

--- src/components/SemTile.tsx
+++ src/components/SemTile.tsx
@@ -4,33 +4,33 @@ interface Subjects {
     subject:string, code:string, ie1:string, ie2:string, ie3:string, finals:string
 }
 
-const SemTile: React.FC<Subjects> = ( subjects ) => {
+const SemTile: React.FC<Subjects> = ( { subject, code, ie1, ie2, ie3, finals } ) => {
   return (
     <div className="text-center my-6 border rounded-lg shadow-md px-4 py-12 m-2 hover:shadow-lg transition duration-300 bg-gray-800">
       <h3 className="my-6 text-3xl font-semibold text-customRed">
-        {subjects.subject}
+        {subject}
       </h3>
-      <p className="my-6 text-gray-400">{subjects.code}</p>
+      <p className="my-6 text-gray-400">{code}</p>
       <a
-        href={subjects.ie1}
+        href={ie1}
         className="bg-customWhite-offWhite p-2 font-bold rounded-xl text-customRed-light hover:text-customRed-dark mr-4"
       >
         IE-1
       </a>
       <a
-        href={subjects.ie2}
+        href={ie2}
         className="bg-customWhite-offWhite p-2 font-bold rounded-xl text-customRed-light hover:text-customRed-dark mr-4"
       >
         IE-2
       </a>
       <a
-        href={subjects.ie3}
+        href={ie3}
         className="bg-customWhite-offWhite p-2 font-bold rounded-xl text-customRed-light hover:text-customRed-dark mr-4"
       >
         IE-3
       </a>
       <a
-        href={subjects.finals}
+        href={finals}
         className="bg-customWhite-offWhite p-2 font-bold rounded-xl text-customRed-light hover:text-customRed-dark"
       >
         Finals
--- src/pages/Semesters.tsx
+++ src/pages/Semesters.tsx
@@ -7,7 +7,7 @@ export const Semester1 = () => {
   return (
     <div className="mx-8 md:grid grid-cols-3">
       {SEM1_SUBJECTS.map((subject, index)=>(
-        <SemTile key={index} subjects={subject}/>
+        <SemTile key={index} {...subject}/>
       ))}
     </div>
   )
@@ -16,7 +16,7 @@ export const Semester2 = () => {
   return (
     <div className="mx-8 md:grid grid-cols-3">
       {SEM2_SUBJECTS.map((subject, index)=>(
-        <SemTile key={index} subjects={subject}/>
+        <SemTile key={index} {...subject}/>
       ))}
     </div>
   )
