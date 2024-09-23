ebb8324216a31ca8dc4fe49aac4ae4472d2af296
Keshav Priyam <keshavpriyam@gmail.com>
2024-09-24 01:05:57 +0530

    pending: fix links - add subjects - add papers

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import Navbar from './Navbar';
 
 const Header:React.FC = () => {
   return (
-    <div className='md:container my-6 md:mx-12'> 
+    <div className='my-6 md:px-12'> 
         <h1 className='font-bold my-6 text-5xl text-customRed-dark text-center'>{info.title}</h1>
         <h1 className='text-sm my-6 text-center mx-8'>{info.desc}</h1>
         <Navbar />
